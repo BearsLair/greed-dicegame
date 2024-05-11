8c43a266b5cec69fadb2240cae135c6350b4f841
Patrick Gross <bears_lair@protonmail.com>
2024-05-11 12:30:00 -0700

    tests created

---
+++ greed.js
@@ -0,0 +1,6 @@
+console.log(score([2, 3, 4, 6, 2])); // 0
+console.log(score([4, 4, 4, 3, 3])); // 400
+console.log(score([2, 4, 4, 5, 4])); // 450
+console.log(score([5, 1, 3, 4, 1])); // 250
+console.log(score([1, 1, 1, 3, 1])); // 1100
+console.log(score([2, 4, 4, 5, 4])); // 450
