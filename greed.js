400ddafb3f9a75397a9474296f9c132f10df2e8a
Patrick Gross <bears_lair@protonmail.com>
2024-05-14 14:43:55 -0700

    scoring algorithm complete

--- greed.js
+++ greed.js
@@ -8,12 +8,68 @@ function score(dice) {
     threeFives: 500,
     threeFours: 400,
     threeThrees: 300,
-    threetwos: 200,
+    threeTwos: 200,
     one1: 100,
     one5: 50,
   };
 
-  dice.map((num) => {});
+  let score = 0;
+
+  let ones = [];
+  let twos = [];
+  let threes = [];
+  let fours = [];
+  let fives = [];
+  let sixes = [];
+
+  dice.map((num) => {
+    switch (num) {
+      case 1:
+        ones.push(num);
+        break;
+      case 2:
+        twos.push(num);
+        break;
+      case 3:
+        threes.push(num);
+        break;
+      case 4:
+        fours.push(num);
+        break;
+      case 5:
+        fives.push(num);
+        break;
+      case 6:
+        sixes.push(num);
+        break;
+    }
+  });
+
+  if ((ones.length = 3)) {
+    total += scoringValues.threeOnes;
+  }
+
+  if ((twos.length = 3)) {
+    total += scoringValues.threeTwos;
+  }
+
+  if ((threes.length = 3)) {
+    total += scoringValues.threeThrees;
+  }
+
+  if ((fours.length = 3)) {
+    total += scoringValues.threeFours;
+  }
+
+  if ((fives.length = 3)) {
+    total += scoringValues.threeFives;
+  }
+
+  if ((sixes.length = 3)) {
+    total += scoringValues.threeSixes;
+  }
+
+  return total;
 }
 
 // console.log(score([2, 3, 4, 6, 2])); // 0
